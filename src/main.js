221c3e62cf635cadc62ab7135f2f11f355c39590
thisismeZML <thatismetheoneudunknow@gmail.com>
2024-02-01 21:02:38 +0630

    Humberger Menu added

--- src/main.js
+++ src/main.js
@@ -21,3 +21,22 @@ dropdownlinks.forEach((link) => {
       link.classList.remove("active");
     });
 })
+
+// humberger
+
+const sideNav = document.getElementById("sideNav");
+const menuBtn = document.getElementById("menu")
+const crossBtn = document.getElementById("cross")
+
+menuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  sideNav.classList.add("translate-x-0")
+  sideNav.classList.add("opacity-100");
+  // sideNav.classList.remove("opacity-0")
+})
+
+crossBtn.addEventListener("click", (e) => {
+  sideNav.classList.remove("translate-x-0");
+  sideNav.classList.remove("opacity-100");
+  // sideNav.classList.add("opacity-0");
+})
