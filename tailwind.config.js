94fe5ab37c1a0b4820783f75a679a0ab8addfd9a
thisismeZML <thatismetheoneudunknow@gmail.com>
2024-02-02 16:57:00 +0630

    curosel and pegenation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
     extend: {
       colors: {
         primaryColor: "#1cb1fe",
       },
+    },
+    container: {
+      center: true,
     },
     fontFamily: {
       primaryFont: ["DM Sans", "sans-serif"],
